5dcec5fbc033f916bd442e0947e6c7ac5838a42d
Pedro Capriles M <p0c04cw@M-C02Z9029LVCG.local>
2021-12-24 09:41:06 -0300

    [Work in Progress] Adding interaction with Mongo DB

--- api/routes/orders/index.js
+++ api/routes/orders/index.js
@@ -1,13 +1,58 @@
-const express = require('express');
+const express = require("express");
+const { StatusCodes } = require("http-status-codes");
+const Orders = require("../../models/Orders");
 const app = express();
 const router = app.router();
 
-router.get('/', (response, request) => {
-    response.send('Hello meals🍔');
+router.get("/", async (request, response) => {
+  const orders = await Orders.find();
+
+  orders && response.status(StatusCodes.OK).send(orders);
 });
 
-router.post('/', (response, request) => {
-    response.send('Post meals 🍱');
+router.get("/:id", async (request, response) => {
+  const { id } = request.params;
+  const order = await Orders.findById(id);
+
+  order && response.status(StatusCodes.OK).send(order);
+});
+
+router.post("/", async (request, response) => {
+  const { name, description } = request.body;
+  const order = await Orders.create({
+    name,
+    description,
+  });
+
+  order && response.status(StatusCodes.CREATED).send(order);
+});
+
+router.post("/", async (request, response) => {
+  const { name, description } = request.body;
+  const order = await Orders.create({
+    name,
+    description,
+  });
+
+  order && response.status(StatusCodes.CREATED).send(order);
+});
+
+router.put("/:id", async (request, response) => {
+  const { name, description } = request.body;
+  const { id } = request.params;
+  await Orders.findOneAndUpdate(id, {
+    name,
+    description,
+  });
+
+  response.sendStatus(StatusCodes.NO_CONTENT);
+});
+
+router.delete("/:id", async (request, response) => {
+  const { id } = request.params;
+  await Orders.findOneAndDelete(id);
+
+  response.sendStatus(StatusCodes.NO_CONTENT);
 });
 
 module.exports = router;
