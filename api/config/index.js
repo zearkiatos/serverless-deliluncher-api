32cd250396810dac8d70f6da2aedc3c48198b640
zearkiatos <caprilespe@outlook.com>
2022-01-14 16:01:04 -0300

    Fix config mapper secret

--- api/config/index.js
+++ api/config/index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const config = {
     ENVIRONMENT: process.env.NODE_ENV,
     PORT: process.env.PORT,
-    MONGO_DATABASE_URI: process.env.MONGO_DATABASE_URI
+    MONGO_DATABASE_URI: process.env.MONGO_DATABASE_URI,
+    EXPIRATION_TOKEN: parseInt(process.env.EXPIRATION_TOKEN),
+    DELILUNCHER_SECRET: process.env.DELILUNCHER_SECRET
 }
 module.exports = config;
